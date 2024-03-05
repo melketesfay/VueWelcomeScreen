425ce177ce3144cdefa66a8f6087edb63f0b2296
melketesfay <melketesfay@gmail.com>
2024-03-05 17:04:25 +0100

    from worklaptop

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Inter", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
